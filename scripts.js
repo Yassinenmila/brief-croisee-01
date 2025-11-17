a0f9fcb24e4c99d81a8b213934d13940d94a2be2
yassinenmila <nmilayassine0000@gmail.com>
2025-11-17 20:18:19 +0100

    fix the layout of the worker unstaff

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ newstf.addEventListener("click", () => {
     const popup = document.createElement('div');
     popup.classList.add('popup');
     popup.innerHTML = `
+    
     <div class="popup-cont">
             <h1>New Worker</h1>
             <div></div>
@@ -15,35 +16,36 @@ newstf.addEventListener("click", () => {
             <label for="name">Nom :</label>
             <input type="text" id="name" placeholder="Entre Votre Nom">
             </div>
-            <div class="inputs">
-            <label for="role">Role :</label>
-            <select name="role" id="role">
-                <option value="Autres">Autres</option>
-                <option value="Réceptionniste">Réceptionniste</option>
-                <option value="Techniciens">Techniciens IT</option>
-                <option value="Agent">Agent de sécurité</option>
-                <option value="Manager">Manager</option>
-                option value="Menage">Menage</option>
-                </select>
-            </div>
-            <div class="inputs">
-             <label for="photo">Photo :</label>
-            <input type="url" id="photo" placeholder="Entre Votre Photo(URL)">
-            </div>
-            <div class="inputs">
-             <label for="email">Email :</label>
-            <input type="text" id="email" placeholder="Entrer Votre Email">
-            </div>
-            <div class="inputs">
-             <label for="tel">Tel :</label>
-            <input type="text" id="tel" placeholder="+2126XXXXXXXXX">
-            </div>
+                <div class="inputs">
+                    <label for="role">Role :</label>
+                    <select name="role" id="role">
+                        <option value="Autres">Autres</option>
+                        <option value="Réceptionniste">Réceptionniste</option>
+                        <option value="Techniciens">Techniciens IT</option>
+                        <option value="Agent">Agent de sécurité</option>
+                        <option value="Manager">Manager</option>
+                        <option value="Menage">Menage</option>
+                    </select>
+                </div>
+                <div class="inputs">
+                    <label for="photo">Photo :</label>
+                    <input type="url" id="photo" placeholder="Entre Votre Photo(URL)">
+                </div>
+                <div class="inputs">
+                    <label for="email">Email :</label>
+                    <input type="text" id="email" placeholder="Entrer Votre Email">
+                </div>
+                <div class="inputs">
+                    <label for="tel">Tel :</label>
+                    <input type="text" id="tel" placeholder="+2126XXXXXXXXX">
+                </div>
             </form>
             <div class="btn-s">
-            <button id="submit" type="submit">Valider</button>
-            <button id="close">close</button>
+                <button id="submit" type="submit">Valider</button>
+                <button id="close">close</button>
             </div>
-            </div>
+    </div>
+    
     `;
     document.body.appendChild(popup);
     document.querySelector("#close").addEventListener("click", () => {
